01d250f20c244fa0da5a4b95cb32f515e5147294
rahul1841 <118290059+rahul1841@users.noreply.github.com>
2024-05-31 14:18:01 +0530

    changed routing ip

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ import uvicorn
 from app.app import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)

83e5de65905dd7c49ff4bb738fc2e72ae1ba26bb
RAGHAV <94848646+raghav1030@users.noreply.github.com>
2024-07-18 01:46:59 +0530

    changed fast api host from 127.0.0.1 to 0.0.0.0

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
 
 
 # from fastapi import FastAPI

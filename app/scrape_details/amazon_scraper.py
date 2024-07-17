0298fe34e703af6c021a569701cea25d4a8cf3f4
RAGHAV <94848646+raghav1030@users.noreply.github.com>
2024-07-18 01:58:08 +0530

    added a new arguement for chromedriver options

--- app/scrape_details/amazon_scraper.py
+++ app/scrape_details/amazon_scraper.py
@@ -174,9 +174,10 @@ class AmazonReviewScraper:
         # Uncomment the following options to run Chrome in headless mode and other configurations if needed
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
-        # options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920x1080")
+
         # options.add_argument("--remote-debugging-port=9222")
         # options.add_argument("--disable-extensions")
         # options.add_argument("--proxy-server='direct://'")
